95bde4076608fabc5fe9524db5f1036fc4c3b981
Asgdhd <151851736+Asgdhd@users.noreply.github.com>
2025-09-12 07:32:39 +0000

    Lab1 1st part

---
+++ sandbox/krakhmalnikova.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Anastasia\nKrakhmalnikova\n");
+    return 0;
+}
---
+++ sandbox/krakhmalnikova64.asm
@@ -0,0 +1,22 @@
+format ELF64
+public _start 
+msg1 db "Anastasia", 0xA, 0 
+msg2 db "Krakhmalnikova", 0xA, 0 
+
+_start:
+    ;инициализация регистров для вывода информации на экран
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, msg1
+    mov rdx, 11
+    int 0x80
+    ;инициализация регистров для вывода информации на экран
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, msg2
+    mov rdx, 16
+    int 0x80
+    ;инициализация регистров для успешного завершения работы программы
+    mov rax, 1
+    mov rbx, 0
+    int 0x80

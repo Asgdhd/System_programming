43f34fd700e9833f9f6a7becd8b6e0b80d479d82
Asgdhd <151851736+Asgdhd@users.noreply.github.com>
2025-09-24 21:06:24 +0000

    Lab2 5th task

---
+++ Lab_2/task5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    long long n = 5607798014ULL;  // Беззнаковый тип
+    int sum = 0;
+    
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    
+    printf("%d\n", sum);
+    return 0;
+}
